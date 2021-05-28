29d848cf5959751f663711e1fbcfeb85050ec3b8
Profotaku <matthieus701@gmail.com>
2021-05-28 19:11:18 +0200

    firstcommit

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta http-equiv="Content-Type"/>
+	<title>Vroom</title>
+    <meta name="description" content="Téléchargez Vroom, c'est inclus avec votre code en ligne!">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+        <div class="container-fluid">
+        
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="fa fa-bars"></span> Menu
+          </button>
+          <div class="collapse navbar-collapse" id="ftco-nav">
+            <ul class="navbar-nav m-auto">
+                <li class="nav-item active"><a href="#" class="nav-link">Accueil</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Démonstration</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Règles & Fonctionnement</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Téléchargement</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Classement</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Réseaux</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Nous contacter</a></li>
+            </ul>
+          </div>
+        </div>
+        </nav>
+        <br>
+        <hr>
+    </header>
+    <h1>Découvrez une nouvelle façon d'apprendre votre code !</h1>
+    <main role="main">
+    <div class="rule">
+        <div class="sliderrule">
+            <figure>
+            <img src="img/beta.jpg" alt="Beta"/>
+            <img src="img/down.jpg" alt="Download"/>
+            </figure>
+        </div>
+    </div>
+    <div class="demo">
+        <div>
+            <figure>
+            <img src="img/demo.jpg" alt="Demo"/>
+            <img src="img/visuel.jpg" alt="Visuel"/>
+            </figure>
+        </div>
+    </div>
+    <div class="down"></div>
+    <div class="classe"></div>
+    <div class="cont">
+        <div class="slidercont">
+            <figure>
+            <img src="img/trophy.jpg" alt="Trophé"/>
+            <img src="img/augmentation.jpg" alt="Classement"/>
+            </figure>
+        </div>
+    </div>
+    <div class="res">
+        <div class="sliderres">
+            <figure>
+            <img src="img/reseaux1.jpg" alt="Facebook" />
+            <img src="img/reseaux2.jpg" alt="Twitter" />
+            <img src="img/reseaux3.jpg" alt="Mail" />
+            </figure>
+        </div>
+    </div>
+    </main>
+    <footer>
+    <div role="footer" class="footer"></div>
+    </footer>
+</body>
+</html>
